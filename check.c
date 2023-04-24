e961b2987a3ad67e33837ffed000f944c6f8c4dc
Hypebeaver <106057661+Hypebeaver@users.noreply.github.com>
2023-04-24 13:24:22 +0800

    Update check.c
    Delete function checkRange() and modify function checkArgument()

--- check.c
+++ check.c
@@ -1,6 +1,4 @@
 /*
- * NAME     : LOOIKIANJOHN
- * ID       : 19741767
  * PURPOSE  : Implement of checking the command line range and argument
  * DATE     : 12/4/23
  * DATE MOD : 17/4/23
@@ -10,23 +8,6 @@
 #include <stdlib.h>
 #include "check.h"
 #include "macro.h"
-
-/* Function to check the argc range from the command line */
-int checkRange(int* range)
-{
-    int correctRange = FALSE;
-
-    if (*range > 7 || *range < 7)
-    {
-        printf("./box <map_row> <map_column> <player_row> <player_column> <goal_row> <goal_column>\n");
-    }
-    else
-    {
-        correctRange = TRUE;
-    }
-
-    return correctRange;
-}
 
 /* Function to check the right argv array from the command line */
 int checkArgument(int* range, char *argv[], int *mRow, int *mCol, int *pRow, int *pCol, int *gRow, int *gCol)
@@ -40,36 +21,48 @@ int checkArgument(int* range, char *argv[], int *mRow, int *mCol, int *pRow, int
     *gRow = atoi(argv[5]);
     *gCol = atoi(argv[6]);
 
-    if (*mRow < 5 || *mCol < 5)
+    if (*range != 7)
+    {
+        printf("Usage: ./box <map_row> <map_column> <player_row> <player_column> <goal_row> <goal_column>\n");
+    }
+    else
     {
         if (*mRow < 0 || *mCol < 0)
         {
             printf("Map range must not be negative\n");
         }
-        else
+        else if (*mRow < 5 || *mCol < 5)
         {
             printf("Minimum map range must be 5 x 5\n");
         }
-    }
-    else if (*mRow > 100 || *mCol > 100)
-    {
-        printf("Maximum map range is 100 x 100\n");
-    }
-    else if (*pRow >= *mRow || *pCol >= *mCol)
-    {
-        printf("Player location must within the map range\n");
-    }
-    else if (*gRow >= *mRow || *gCol >= *mCol)
-    {
-        printf("Goal location must within the map range\n");
-    }
-    else if ( ISEQUAL(*pRow, *pCol, *gRow, *gCol) == TRUE )
-    {
-        printf("Player location and goal location must not be the same\n");
-    }
-    else
-    {
-        correctArg = TRUE;
+        else if (*mRow > 100 || *mCol > 100)
+        {
+            printf("Maximum map range is 100 x 100\n");
+        }
+        else if (*pRow < 0 || *pCol < 0)
+        {
+            printf("Player row and column must not be negative\n");
+        }
+        else if (*pRow >= *mRow || *pCol >= *mCol)
+        {
+            printf("Player location must within the map range\n");
+        }
+        else if (*gRow < 0 || *gCol < 0)
+        {
+            printf("Goal row and column must not be negative\n");
+        }
+        else if (*gRow >= *mRow || *gCol >= *mCol)
+        {
+            printf("Goal location must within the map range\n");
+        }
+        else if ( ISEQUAL(*pRow, *pCol, *gRow, *gCol) == TRUE )
+        {
+            printf("Player location and goal location must not be the same\n");
+        }
+        else
+        {
+            correctArg = TRUE;
+        }
     }
 
     return correctArg;
