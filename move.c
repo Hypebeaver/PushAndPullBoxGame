95ec5b4c86aec4f4823595355e31f16f55455909
Hypebeaver <106057661+Hypebeaver@users.noreply.github.com>
2023-04-24 13:29:45 +0800

    Update move.c
    Update the explanation and add function pull().

--- move.c
+++ move.c
@@ -1,6 +1,4 @@
 /*
- * NAME     : LOOIKIANJOHN
- * ID       : 19741767
  * PURPOSE  : Implement of receiving command line argument to execute the program
  * DATE     : 12/4/23
  * DATE MOD : 17/4/23
@@ -9,7 +7,10 @@
 #include <stdio.h>
 #include "move.h"
 
-/* Function that move the player with specific character within the location of the map */
+/* Function that move the player with specific character within the location of the map.
+   Player will move around with 'w', 'a', 's' and 'd' characters.
+   If-else is used to determine the player move with the input character. 
+   Another If-else is also implement to check the player is +1/-1 next to the border or not. */
 void move(char* wasd, int* mRow, int* mCol, int* pRow, int* pCol)
 {
     if (*wasd == 'w')
@@ -42,7 +43,11 @@ void move(char* wasd, int* mRow, int* mCol, int* pRow, int* pCol)
     }
 }
 
-/* Function that push the box with specific character within  location of the map*/
+/* Function that push the box by the player within the location of the map 
+   Player will move around with 'w', 'a', 's' and 'd' characters.
+   If-else is used to determine the box move with the input character. 
+   Another If-else is also implement to check the box is +1/-1 next to the border or not.
+   If the box and player is at the border of the map and at the same position, player will -1/+1 back to previous position. */
 void push(char* wasd, int* mRow, int* mCol, int* pRow, int* pCol, int* bRow, int* bCol)
 {
     if (*wasd == 'w')
@@ -90,3 +95,39 @@ void push(char* wasd, int* mRow, int* mCol, int* pRow, int* pCol, int* bRow, int
         }
     }
 }
+
+/* Function that let the player to pull the box within the location of the map
+   If-else is used to determine the row or column of the player is away from the box or not. 
+   If it is, then another If-else will undergo to check whether the box is +2/-2 next to the border. */
+void pull(int* mRow, int* mCol, int* pRow, int* pCol, int* bRow, int* bCol)
+{
+    if (*pRow < *bRow)
+    {
+        if (*bRow > 2)
+        {
+            *bRow = *bRow - 1;
+        }
+    }
+    else if (*pRow > *bRow)
+    {
+        if (*bRow < *mRow - 3)
+        {
+            *bRow = *bRow + 1;
+        }
+    }
+    else if (*pCol < *bCol)
+    {
+        if (*bCol > 2)
+        {
+            *bCol = *bCol - 1;
+        }
+    }
+    else
+    {
+        if (*bCol < *mCol - 3)
+        {
+            *bCol = *bCol + 1;
+        }
+    }
+}
+
